06df476371090616993419fabadf6412ab0b0b71
ReinerHuechting <46283923+ReinerHuechting@users.noreply.github.com>
2022-03-22 23:50:38 +0100

    Hilfsfunktionen für's Sortieren in eigene Datei verschoben.

--- Main.java
+++ Main.java
@@ -6,65 +6,6 @@ class Main {
   }
 
  /**
-  * Erwartet zwei Listen und prüft, ob sie gleich sind.
-  */
-  public static boolean listsAreEqual(List l1, List l2) {
-    // TODO
-    return false;
-  }
-
- /** Erwartet eine Liste und prüft, ob diese sortiert ist. */
-  public static boolean isSorted(List list) {
-    // TODO
-    return false;
-  }
-
- /**
-  * Vertauscht die Elemente an den Positionen i und j.
-  */
-  public static void swap(List list, int i, int j) {
-    // TODO
-  }
-
-  /**
-  * Läuft von links nach rechts durch die Liste und vergleicht dabei jedes Element
-  * mit seinem rechten Nachbarn.
-  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
-  */
-  public static void bubbleUp(List list) {
-    // TODO
-  }
-
-  /**
-  * Läuft von links nach rechts durch die Liste und vergleicht dabei jedes Element
-  * mit seinem rechten Nachbarn.
-  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
-  * Startet bei Position start und stoppt, sobald einmal nicht vertauscht wurde.
-  */
-  public static void bubbleUp(List list, int start) {
-    // TODO
-  }
-
-  /**
-  * Läuft von rechts nach links durch die Liste und vergleicht dabei jedes Element
-  * mit seinem linken Nachbarn.
-  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
-  */
-  public static void bubbleDown(List list) {
-    // TODO
-  }
-
- /**
-  * Läuft von rechts nach links durch die Liste und vergleicht dabei jedes Element
-  * mit seinem linken Nachbarn.
-  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
-  * Startet bei Position start und stoppt, sobald einmal nicht vertauscht wurde.
-  */
-  public static void bubbleDown(List list, int start) {
-    // TODO
-  }
-
- /**
   * Sucht das kleinste Element in der Liste und gibt dessen Position
   * zurück. Startet erst ab Position start.
   */
--- Test.java
+++ Test.java
@@ -43,7 +43,7 @@ public class Test {
   * Gibt eine Fehlermeldung aus, falls sie es nicht sind.
   */
   public static void assertListsAreEqual(List l1, List l2) {
-    if (!Main.listsAreEqual(l1, l2)) {
+    if (!SortHelpers.listsAreEqual(l1, l2)) {
       System.out.println("Fehler, Listen sollten gleich sein, sind es aber nicht.");
       System.out.println("l1: " + l1.toString());
       System.out.println("l2: " + l2.toString());
