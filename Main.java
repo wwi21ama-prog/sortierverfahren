7316a9b260bfd3524c2c383ab53d4e37a4950982
Reiner Hüchting <reiner.huechting@gmx.de>
2022-03-22 17:48:18 +0000

    Weitere Hilfsfunktionen formuliert.

--- Main.java
+++ Main.java
@@ -15,11 +15,31 @@ class Main {
   }
 
   /**
+  * Läuft von links nach rechts durch die Liste und vergleicht dabei jedes Element
+  * mit seinem rechten Nachbarn.
+  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
+  * Startet bei Position start und stoppt, sobald einmal nicht vertauscht wurde.
+  */
+  public static void bubbleUp(List list, int start) {
+    // TODO
+  }
+
+  /**
   * Läuft von rechts nach links durch die Liste und vergleicht dabei jedes Element
   * mit seinem linken Nachbarn.
   * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
   */
   public static void bubbleDown(List list) {
+    // TODO
+  }
+
+ /**
+  * Läuft von rechts nach links durch die Liste und vergleicht dabei jedes Element
+  * mit seinem linken Nachbarn.
+  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
+  * Startet bei Position start und stoppt, sobald einmal nicht vertauscht wurde.
+  */
+  public static void bubbleDown(List list, int start) {
     // TODO
   }
     
