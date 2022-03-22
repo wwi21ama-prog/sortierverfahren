57d09f411bb26a59edd01719c24487ab3fee804d
Reiner Hüchting <reiner.huechting@gmx.de>
2022-03-22 17:45:42 +0000

    Main-Klasse repariert und Bubble-Aufgaben formuliert.

--- Main.java
+++ Main.java
@@ -1,23 +1,32 @@
-
+import java.util.List;
 
 class Main {
   public static void main(String[] args) {
     System.out.println("Hallo Welt");
   }
-}
 
-public static void bubbleUp(List list) {
-  // TODO
-
-  /*
-  Hinweis: Wenn Sie unsere List-Datenstruktur nehmen, müssen Sie noch
-           einen Vergleich zwischen Elementen implementieren.
+  /**
+  * Läuft von links nach rechts durch die Liste und vergleicht dabei jedes Element
+  * mit seinem rechten Nachbarn.
+  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
   */
-}
-  
-public static void bubbleSort(List list) {
-  // TODO
-  // In einer Schleife so oft bubbleUp() aufrufen, wie nötig.
-  // list.size() mal.
-  // Evtl. Optimierungen einbauen.
+  public static void bubbleUp(List list) {
+    // TODO
+  }
+
+  /**
+  * Läuft von rechts nach links durch die Liste und vergleicht dabei jedes Element
+  * mit seinem linken Nachbarn.
+  * Wenn die beiden Elemente falsch sortiert sind, werden sie vertauscht.
+  */
+  public static void bubbleDown(List list) {
+    // TODO
+  }
+    
+  public static void bubbleSort(List list) {
+    // TODO
+    // In einer Schleife so oft bubbleUp() aufrufen, wie nötig.
+    // list.size() mal.
+    // Evtl. Optimierungen einbauen.
+  }
 }
